c9eca7cc9056a512127e3d389762a702b9911b17
ArturoZAC <arturoyz2105@gmail.com>
2025-02-09 21:40:27 -0500

    9

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,20 +1,42 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
+const seedProducts = initialData.products;
+
 interface Props {
-  params: Promise<{id: string}>
+  params: Promise<{id: Category}>
 }
 
 export default async function CategoryPage({ params }: Props) {
 
   const { id } = await params;
+  const products = seedProducts.filter( product => product.gender === id);
 
-  if ( id === 'kids' ) {
-    notFound();
-  } 
+  const labels: Record<Category, string> = {
+    'men': 'para hombres',
+    'women': 'para mujeres',
+    'kid': 'para niños',
+    'unisex': "para todos",
+  }
+
+  // if ( id === 'kids' ) {
+  //   notFound();
+  // } 
 
   return (
-    <div>
-      <h1>Category Page { id } </h1>
-    </div>
+    <>
+      <Title 
+        title={`Articulos de ${ labels[id] }`}
+        subtitle="Todos los productos"
+        className="mb-2"
+
+      />
+
+      <ProductGrid
+        products={ products }
+      />
+    </>
   );
 }
--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,5 +1,20 @@
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
 
-export default function ProductPage() {
+
+interface Props {
+  params: Promise<{slug: string}>
+}
+
+export default async function ProductPage({ params }: Props) {
+
+  const { slug } = await params;  
+  const product = initialData.products.find( product => product.slug === slug );
+
+  if( !product ) {
+    notFound();
+  }
+
   return (
     <div>
       <h1>Product Page</h1>
