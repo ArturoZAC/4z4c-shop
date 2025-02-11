f1c759289a3b341b201db20689c9583654978c84
ArturoZAC <arturoyz2105@gmail.com>
2025-02-11 16:53:48 -0500

    11

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { ProductSlideShow, QuantitySelector, SizeSelector } from "@/components";
+import { ProductMobileSlideShow, ProductSlideShow, QuantitySelector, SizeSelector } from "@/components";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
@@ -21,10 +21,20 @@ export default async function ProductPage({ params }: Props) {
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
 
       <div className="col-span-1 md:col-span-2">
+
+        <ProductMobileSlideShow 
+          images={ product.images }
+          title={ product.title }
+          className="block md:hidden"
+        />
+
         <ProductSlideShow
           images={ product.images }
           title={ product.title }
+          className="hidden md:block"
         />
+
+
       </div>
 
       <div className="col-span-1 px-5">
